17cd39d369af448bcdaad2cc00cdd134dfef2789
notfound945 <732039303@qq.com>
2021-07-25 13:24:29 +0800

    initial project

---
+++ README.md
@@ -0,0 +1,56 @@
+# 以下为旧版内容，现已修改为企业微信推送
+
+
+## Serve酱自动推送热门
+
+> 🔝 通过Serve酱自动发送微博、知乎、v2ex热门内容到微信，可配置 workflow 的触发条件为 schedule，实现周期性定时发送热门内容。
+
+### 示例
+
+clone 此 GitHub 仓库，修改.github/workflows/ 文件夹下一个 main.yml 文件，内容如下：
+
+```yml
+name: 'Push News'
+
+on:
+  push:
+    branches:
+      - master
+  schedule:
+    - cron: '0 */3 * * *' # 定义 cron 表达式
+  watch:
+    types: [started] # 定义star是自动发送
+
+env:
+  TZ: Asia/Shanghai
+
+jobs:
+  Gitfolio-Spider:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: 'Set up Python'
+        uses: actions/setup-python@v1
+        with:
+          python-version: 3.8
+      - name: 'Install dependencies'
+        run: python -m pip install --upgrade pip
+      - name: 'Install requirements'
+        run: pip install -r ./requirements.txt
+      - name: 'Working'
+        env:
+          SECRET: ${{ secrets.SECRET }}
+        timeout-minutes: 350
+        run: |
+          echo SECRET=$SECRET > .env
+          python main.py
+          rm -f .env
+
+```
+
+**注意**
+
+- cron 是 UTC 时间，使用时请将北京时间转换为 UTC 进行配置。
+- 请在项目的 Settings -> Secrets 路径下配置好SECRET(server酱密钥)，不要直接在 .yml 文件中暴露地址跟密钥
+- SECRET 设置为 `xxxxxx`
---
+++ main.py
@@ -0,0 +1,267 @@
+import asyncio
+import json
+import os
+import random
+from datetime import datetime
+from urllib import parse
+
+import httpx
+from dotenv import find_dotenv, load_dotenv
+
+USER_AGENTS = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; "
+    "SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; "
+    "SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; "
+    "Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; "
+    "Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; "
+    ".NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; "
+    "Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; "
+    ".NET CLR 3.5.30729; .NET CLR 3.0.30729; "
+    ".NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; "
+    "Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; "
+    "InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) "
+    "AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) "
+    "Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ "
+    "(KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; "
+    "rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) "
+    "Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) "
+    "Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) "
+    "Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 "
+    "(KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) "
+    "AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) "
+    "Presto/2.9.168 Version/11.52"
+]
+TIMEOUT = 5
+
+
+class Action:
+    def __init__(self):
+        load_dotenv(find_dotenv(), override=True)
+
+        self.corpid = os.environ.get('CORPID', '')
+        self.agentid = os.environ.get('AGENTID', '')
+        self.thumbmediaid = os.environ.get('THUMBMEDIAID', '')
+        self.corpsecret = os.environ.get('CORPSECRET', '')
+
+        self.contents = []
+        self.res = False
+        self.task_list = []
+
+    async def qywx(self):
+        """ 企业微信推送 """
+        dt = datetime.now()
+        date = dt.strftime('%Y-%m-%d')
+        time = dt.strftime('%H:%M:%S')
+        url = f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={self.corpid}&corpsecret={self.corpsecret}'
+        try:
+            resp = await self.client.get(url)
+            access_token = resp.json()['access_token']
+            send_url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
+            try:
+                data = {
+                    'touser': f'@all',
+                    'agentid': self.agentid,
+                    'msgtype': 'mpnews',
+                    'mpnews': {
+                        'articles': [{
+                            'title': f'热搜榜 - {date}',
+                            'thumb_media_id': self.thumbmediaid,
+                            'author': 'notfound945',
+                            'digest': f'{time} - 热搜消息',
+                            'content': f'{"".join(self.contents)}'
+                        }
+                        ]
+                    }
+                }
+                res = await self.client.post(send_url, data=json.dumps(data), timeout=TIMEOUT)
+                print(res.json())
+
+            except Exception as e:
+                print(f'something error occurred, message: {e}')
+
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+        
+
+
+    async def servechan(self):
+        """ Server酱推送 """
+        dt = datetime.now()
+        time = dt.strftime('%Y-%m-%d')
+        url = f'https://sc.ftqq.com/{self.secret}.send'
+        data = {
+            'text': f'资讯热文推送-{time}',
+            'desp': f'{"".join(self.contents)}'
+        }
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        try:
+            resp = await self.client.post(url, headers=headers,
+                                          data=parse.urlencode(data), timeout=TIMEOUT)
+            self.res = resp.json()['errno'] == 0
+            print(resp.text)
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+    async def get_v2ex_hot_topics(self):
+        url = 'https://www.v2ex.com/api/topics/hot.json'
+        headers = {'User-Agent': random.choice(USER_AGENTS)}
+        try:
+            resp = await self.client.get(url, headers=headers, timeout=TIMEOUT)
+            self.contents.append(f'<h2> v2ex热门主题 </h2>')
+            for item in resp.json()[:10]:
+                detail_url = item['url']
+                title = item['title']
+                content = f'<ul><li> <a href={detail_url}>{title}</a> </li></ul>'
+                self.contents.append(content)
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+    async def get_zhihu_hot_topics(self):
+        url = "https://bicido.com/api/news/?type_id=4"
+        headers = {
+            'Referer': 'https://bicido.com/',
+            'Host': 'bicido.com',
+            'User-Agent': random.choice(USER_AGENTS)
+        }
+        try:
+            resp = await self.client.get(url, headers=headers, timeout=TIMEOUT)
+            self.contents.append(f'\n> 知乎热搜 \n\n')
+            for item in resp.json()[:10]:
+                detail_url = item['source_url']
+                title = item['title']
+                # content = f'- [{title}]({detail_url})\n'
+                content = f'<ul><li> <a href={detail_url}>{title}</a> </li></ul>'
+                self.contents.append(content)
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+    async def get_weibo_hot_search(self):
+        url = "https://bicido.com/api/news/?type_id=1"
+        headers = {
+            'Referer': 'https://bicido.com/',
+            'Host': 'bicido.com',
+            'User-Agent': random.choice(USER_AGENTS)
+        }
+        try:
+            resp = await self.client.get(url, headers=headers, timeout=TIMEOUT)
+            self.contents.append(f'\n> 微博热搜榜\n\n')
+            for item in resp.json()[:10]:
+                detail_url = item['source_url']
+                title = item['title']
+                content = f'<ul><li> <a href={detail_url}>{title}</a> </li></ul>'
+                # content = f'- [{title}]({detail_url})\n'
+                self.contents.append(content)
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+    async def get_weibo_hot_topics(self):
+        url = "https://bicido.com/api/news/?type_id=2"
+        headers = {
+            'Referer': 'https://bicido.com/',
+            'Host': 'bicido.com',
+            'User-Agent': random.choice(USER_AGENTS)
+        }
+        try:
+            resp = await self.client.get(url, headers=headers, timeout=TIMEOUT)
+            self.contents.append(f'\n> 微博话题榜\n\n')
+            for item in resp.json()[:10]:
+                detail_url = item['source_url']
+                title = item['title']
+                # content = f'- [{title}]({detail_url})\n'
+                content = f'<ul><li> <a href={detail_url}>{title}</a> </li></ul>'
+                self.contents.append(content)
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+    async def get_douban_hot_topics(self):
+        url = "https://bicido.com/api/news/?type_id=5"
+        headers = {
+            'Referer': 'https://bicido.com/',
+            'Host': 'bicido.com',
+            'User-Agent': random.choice(USER_AGENTS)
+        }
+        try:
+            resp = await self.client.get(url, headers=headers, timeout=TIMEOUT)
+            self.contents.append(f'\n> 豆瓣话题\n\n')
+            for item in resp.json()[:10]:
+                detail_url = item['source_url']
+                title = item['title']
+                # content = f'- [{title}]({detail_url})\n'
+                content = f'<ul><li> <a href={detail_url}>{title}</a> </li></ul>'
+                self.contents.append(content)
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+    async def get_github_trend(self):
+        url = "https://github-trending.vercel.app/repo"
+        headers = {'User-Agent': random.choice(USER_AGENTS)}
+        try:
+            resp = await self.client.get(url, headers=headers, timeout=TIMEOUT)
+            self.contents.append(f'\n> github热榜\n\n')
+            for item in resp.json()['items'][:10]:
+                detail_url = item['repo_link']
+                title = item['repo']
+                content = f'<ul><li> <a href={detail_url}>{title}</a> </li></ul>'
+                # content = f'- [{title}]({detail_url})\n'
+                self.contents.append(content)
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+    async def get_fish_trend(self):
+        url = "https://www.tophub.fun:8888/GetAllInfoGzip?id=1006"
+        headers = {
+            'Referer': 'https://mo.fish/',
+            'Origin': 'https://mo.fish',
+            'User-Agent': random.choice(USER_AGENTS)
+        }
+        try:
+            resp = await self.client.get(url, headers=headers, timeout=TIMEOUT)
+            self.contents.append(f'\n> 鱼塘热榜\n\n')
+            for item in resp.json()['Data']:
+                detail_url = item['Url']
+                title = item['Title']
+                type = item['type']
+                content = f'<ul><li> <a href={detail_url}>{title}</a> </li></ul>'
+                # content = f'- [{title}]({detail_url}) [{type}]\n'
+                self.contents.append(content)
+        except Exception as e:
+            print(f'something error occurred, message: {e}')
+
+    async def run(self):
+        """主方法"""
+        async with httpx.AsyncClient() as client:
+            self.client = client
+            self.task_list.append(asyncio.create_task(self.get_weibo_hot_topics()))
+            self.task_list.append(asyncio.create_task(self.get_weibo_hot_search()))
+            self.task_list.append(asyncio.create_task(self.get_zhihu_hot_topics()))
+            self.task_list.append(asyncio.create_task(self.get_douban_hot_topics()))
+            self.task_list.append(asyncio.create_task(self.get_v2ex_hot_topics()))
+            self.task_list.append(asyncio.create_task(self.get_github_trend()))
+            self.task_list.append(asyncio.create_task(self.get_fish_trend()))
+
+            await asyncio.gather(*self.task_list)
+            # await self.servechan()
+            await self.qywx()
+            # print(f'{"".join(self.contents)}')
+
+
+if __name__ == '__main__':
+    action = Action()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(action.run())
